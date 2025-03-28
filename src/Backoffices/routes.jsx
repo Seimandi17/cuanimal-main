5344ed20be168c31cfa7380683c8351f35fc4dc2
Valentín <vale.seimandi@hotmail.com>
2025-03-27 21:03:10 -0300

    cambios en rutas

--- src/Backoffices/routes.jsx
+++ src/Backoffices/routes.jsx
@@ -1,12 +1,8 @@
 import { Route } from "react-router-dom";
 import BackofficesLayout from "./layouts/BackofficesLayouts";
 import Home from "./Home";
-import Client from "./moduls/Clientes";
-import Provider from "./moduls/Provider";
 import ListaClientes from "./pages/admin/Clientes/ListaClientes";
-import DetalleCliente from "./pages/admin/Clientes/DetalleCliente";
 import ListaProveedores from "./pages/admin/Proveedores/ListaProveedores";
-import DetalleProveedor from "./pages/admin/Proveedores/DetalleProveedor";
 import ServiciosOfrecidos from "./pages/admin/Servicios/ServiciosOfrecidos";
 import Configuracion from "./pages/admin/Configuracion";
 import MisServicios from "./pages/provider/MisServicios";
@@ -18,13 +14,9 @@ import ValidarCuentas from "./pages/admin/ValidarCuentas";
 export const RoutesBackoffices = [
   <Route key="/backoffice" path="/backoffice" element={<BackofficesLayout />}>
     <Route index element={<Home />} />
-    <Route path="clientes" element={<Client />} />
-    <Route path="proveedor" element={<Provider />} />
     <Route path="admin/validaciones" element={<ValidarCuentas />} />
     <Route path="admin/clientes/lista-cliente" element={<ListaClientes />} />
-    <Route path="admin/clientes/:id" element={<DetalleCliente />} />
     <Route path="admin/proveedores/lista-proveedores" element={<ListaProveedores />} />
-    <Route path="admin/proveedores/:id" element={<DetalleProveedor />} />
     <Route path="admin/servicios" element={<ServiciosOfrecidos />} />
     <Route path="admin/configuracion" element={<Configuracion />} />
     <Route path="proveedor/mis-servicios" element={<MisServicios />} />
